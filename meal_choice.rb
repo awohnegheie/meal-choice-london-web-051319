21d3a197d685e243cc9670f108cf29665bfef1ef
Awoh Negheie <awohnegheie@gmail.com>
2019-05-10 03:21:18 +0000

    Automatically backed up by Learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,9 +1,5 @@
 # Define a method meal_choice that returns the meal_choice that was
 # passed into it and defaults to meat.
-class Meal
-  attr_reader :choice
-
-   def initialize(choice = "meat")
-    @choice = choice
-  end
+cdef meal_choice(meal_choice = "meat")
+  return "#{meal_choice}"
 end
